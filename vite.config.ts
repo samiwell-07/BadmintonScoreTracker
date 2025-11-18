44ff49f001fcfdc3cb31e7571eda788d8d446cf3
Samuel Srouji <samuel.srouji@outlook.com>
2025-11-17 22:08:05 -0500

    Fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = 'BadmintonScroreTracker'
-const isGitHubPages = process.env.GITHUB_PAGES === 'true'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: isGitHubPages ? `/${repoName}/` : '/',
+  base: '/BadmintonScoreTracker/',
 })
